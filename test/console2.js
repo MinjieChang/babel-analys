32ac1ec9f09ca7a9860684a7b066d778676e1fec
changminjie <changminjie@meituan.com>
2021-05-10 20:23:50 +0800

    update: 添加脚本

---
+++ test/console2.js
@@ -0,0 +1,25 @@
+// 使用 babel-cli 配合 .babelrc 编译 
+
+let a = b => {
+  /*
+  * no remove
+  * lkkkkkkk
+  */
+  console.log(b)
+};
+let fn = c => { console.log(c)}
+function code(x) {
+  console.log(x) // reserve
+  if (true) {
+    // 哈哈发寒风
+    console.log(x) // no remove
+    // hhhhhhh
+    console.log(x)
+  }
+  // ggggg
+  console.log(x)
+  // reserve
+  console.log(y)
+  // return number
+  return x * x
+};
